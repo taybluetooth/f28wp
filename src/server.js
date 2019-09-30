f12cf49d2d58aba5ace4d1f1130fd0e28b2ceab5
Jacob-Mowat <zaxquit@protonmail.com>
2019-09-30 12:56:19 +0100

    Added nunjucks configuration

--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const nunjucks = require('nunjucks')
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
