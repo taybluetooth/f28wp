eafa2be3c579e8bfc674b90c2ecc008451d88071
Jacob-Mowat <zaxquit@protonmail.com>
2019-09-30 14:49:25 +0100

    Updated server to use static files

--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@ const app = express()
 const port = 3000
 
 const nunjucks = require('nunjucks')
-nunjucks.configure('views', {
+nunjucks.configure('src/views', {
     autoescape: true,
     express: app
 });
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('src/static'))
+
+app.get('/', (req, res) => res.render('base.njk'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
