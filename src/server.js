19925c06377b7f24856a61c9e2c2c36206128a4f
taybluetooth <callumtaylorbusiness@gmail.com>
2019-10-03 13:11:57 +0100

    Server map refactored with consideration for assets added.

--- src/server.js
+++ src/server.js
@@ -2,24 +2,11 @@ const express = require('express')
 const app = express()
 var path = require('path')
 
-<<<<<<< HEAD
 app.use(express.static(__dirname + '/public'));
-=======
-const nunjucks = require('nunjucks')
-nunjucks.configure('src/views', {
-    autoescape: true,
-    express: app
-});
-
-app.use(express.static('src/static'))
-
-app.get('/', (req, res) => res.render('base.njk'))
->>>>>>> 063b0a702228ce0d6b5068b0c316c5d113060f10
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + 'index.html');
 });
-// Router added for sitemap potential.
 
 app.listen(process.env.port || 3000);
 console.log('BEST BOY TANK LISTENING ON PORT 3000');
