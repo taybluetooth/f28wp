5bf4ad2ef5bf1b1ed62da4442b3da10247912cbd
taybluetooth <callumtaylorbusiness@gmail.com>
2019-10-17 13:06:54 +0100

    Updated ReadMe

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,33 +1,34 @@
 var canvas = document.getElementById("canvas")
-var width = window.innerWidth
-var height = window.innerHeight
-canvas.width = width;
-canvas.height = height;
 var ctx = canvas.getContext("2d")
-var camera = new Camera(ctx)
-window.addEventListener("keydown", keydown, false)
-window.addEventListener("keyup", keyup, false)
+var camera = new Camera(ctx);
+var width
+var height
 
-var keyMap = {
-  68: 'right',
-  65: 'left',
-  87: 'up',
-  83: 'down'
+var resize = function() {
+  width = window.innerWidth * 2
+  height = window.innerHeight * 2
+  canvas.width = width
+  canvas.height = height
 }
+window.onresize = resize
+resize()
 
 var state = {
-  x: width / 2,
-  y: height / 2,
+  position: {
+    x: (width / 2),
+    y: (height / 2)
+  },
+  movement: {
+    x: 0,
+    y: 0
+  },
+  rotation: 0,
   pressedKeys: {
     left: false,
     right: false,
     up: false,
     down: false
   }
-}
-
-function updateText() {
-  document.getElementById("coord").innerHTML = "Current Coordinates: X: " + state.x.toFixed(2) + " Y: " + state.y.toFixed(2)
 }
 
 function background() {
@@ -43,6 +44,91 @@ function loadImg(name) {
   return img;
 }
 
+function update(progress) {
+  var p = progress * 3
+
+  updateRotation(p)
+  updateMovement(p)
+  updatePosition(p)
+
+  camera.moveTo(state.position.x, state.position.y)
+}
+
+function updateRotation(p) {
+  if (state.pressedKeys.left) {
+    state.rotation -= p / 5
+  }
+  else if (state.pressedKeys.right) {
+    state.rotation += p / 5
+  }
+}
+
+function updateMovement(p) {
+
+  var accelerationVector = {
+    x: p * 0.2 * Math.cos((state.rotation-90) * (Math.PI/180)),
+    y: p * 0.2 * Math.sin((state.rotation-90) * (Math.PI/180))
+  }
+
+  if (state.pressedKeys.up) {
+    state.position.x += accelerationVector.x
+    state.position.y += accelerationVector.y
+  }
+  else if (state.pressedKeys.down) {
+    state.position.x -= accelerationVector.x
+    state.position.y -= accelerationVector.y
+  }
+
+}
+
+function updatePosition(p) {
+
+  // Detect boundaries
+  if (state.position.x > width-40) {
+    state.position.x -= p/5
+  }
+  else if (state.position.x < 40) {
+    state.position.x += p/5
+  }
+  if (state.position.y > height-40) {
+    state.position.y -= p/5
+  }
+  else if (state.position.y < 40) {
+    state.position.y += p/5
+  }
+}
+
+function draw() {
+  ctx.clearRect(0, 0, width, height)
+  camera.begin()
+  ctx.fillStyle = background()
+  ctx.fillRect(-500, -500, 10000, 10000)
+  ctx.save();
+  ctx.translate(state.position.x, state.position.y)
+  ctx.rotate((Math.PI/180) * state.rotation)
+  ctx.drawImage(loadImg('tank'), 0, 0, 40, 40)
+  ctx.restore()
+  camera.end();
+}
+
+function loop(timestamp) {
+  var progress = timestamp - lastRender
+
+  update(progress)
+  draw()
+
+  lastRender = timestamp
+  window.requestAnimationFrame(loop)
+}
+var lastRender = 0
+window.requestAnimationFrame(loop)
+
+var keyMap = {
+  68: 'right',
+  65: 'left',
+  87: 'up',
+  83: 'down'
+}
 function keydown(event) {
   var key = keyMap[event.keyCode]
   state.pressedKeys[key] = true
@@ -52,57 +138,5 @@ function keyup(event) {
   state.pressedKeys[key] = false
 }
 
-function update(progress) {
-  if (state.pressedKeys.left) {
-    state.x -= progress/3
-  }
-  if (state.pressedKeys.right) {
-    state.x += progress/3
-  }
-  if (state.pressedKeys.up) {
-    state.y -= progress/3
-  }
-  if (state.pressedKeys.down) {
-    state.y += progress/3
-  }
-  if (state.x > 1000) {
-    state.x -= 1000
-  }
-  else if (state.x < 0) {
-    state.x += 1000
-  }
-  if (state.y > 1000) {
-    state.y -= 1000
-  }
-  else if (state.y < 0) {
-    state.y += 1000
-  }
-  camera.moveTo(state.x, state.y)
-}
-
-function draw() {
-  ctx.clearRect(0, 0, width, height)
-  camera.begin()
-  ctx.fillStyle = background()
-  ctx.fillRect(-1920, -1920, 10000, 10000)
-  ctx.font = "10px Poppins";
-  ctx.fillStyle = "white";
-  ctx.textAlign = "center";
-  ctx.fillText("Bob", state.x + 20, state.y + 50);
-  ctx.drawImage(loadImg('tank'), state.x, state.y, 40, 40)
-  camera.end()
-}
-
-function loop(timestamp) {
-  var progress = timestamp - lastRender
-
-  update(progress)
-  updateText()
-  draw()
-
-  lastRender = timestamp
-  window.requestAnimationFrame(loop)
-}
-
-var lastRender = 0
-window.requestAnimationFrame(loop)
+window.addEventListener("keydown", keydown, false)
+window.addEventListener("keyup", keyup, false)
