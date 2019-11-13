ef05670ebeaf36df52a7e49dc16d5404ea396c55
Jacob-Mowat <zaxquit@protonmail.com>
2019-11-13 00:27:33 +0000

    Implemented endpoint call for /get/session_id, creates a UUID4 value

--- src/server.js
+++ src/server.js
@@ -2,10 +2,22 @@ const express = require('express')
 const app = express()
 var path = require('path')
 
-app.use(express.static(__dirname + '/public'));
+var uuid4 = require('uuid4')
+const id = uuid4();
+if(!uuid4.valid(id)) {
+	console.log('[FAIL] UUID not created!');
+}
+
+var nunjucks = require('nunjucks')
+nunjucks.configure(__dirname + '/views/');
+// console.log(__dirname + '/views/');
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + 'index.html');
+  	nunjucks.render('index.html', { sessionId: id });
+});
+
+app.get('/get/session_id', (req, res) => {
+	res.send(id);
 });
 
 app.listen(process.env.port || 3000);
