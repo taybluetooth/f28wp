7d4a6cf0e6ab7d3053c0b0c95f418f49b096d3ca
HDMcGrath <43671042+HDMcGrath@users.noreply.github.com>
2019-11-13 00:32:13 +0000

    comments in file for future changes
    Don't need to create a db everytime code is ran, it should exist on the server with the rest of the website. Use cookies or localStorage locally, cookies is standard to allow opting out.

--- src/public/js/database.js
+++ src/public/js/database.js
@@ -1,5 +1,6 @@
 var mysql = require ('mysql');
-
+##This code needs deleted, use this file to push code to database on server, get info from cookies from each users session. 
+##push code up every 5 seconds for testing, use score tied to time as a good indicator its working.
 var con = mysql.connection({
   host: "localhost",
   user: "root",
