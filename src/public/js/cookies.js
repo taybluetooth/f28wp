3fdfecc94f0c33fb4e3da86a4ff6635c974ee936
HDMcGrath <43671042+HDMcGrath@users.noreply.github.com>
2019-11-13 00:29:11 +0000

    added cookies.js
    temp cookies class, will need worked on for correct values to be stored as well as potentially push to db

---
+++ src/public/js/cookies.js
@@ -0,0 +1,6 @@
+function setCookie(cName, cID, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        var expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cID + ";" + expires + ";path=/"
+}
