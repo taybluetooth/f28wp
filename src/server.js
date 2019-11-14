9cc7d6b312c8e0480af32276f83bdbb1f518cd86
Jacob-Mowat <zaxquit@protonmail.com>
2019-11-14 03:32:19 +0000

    Fixed ssl certificate location

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
-var privateKey  = fs.readFileSync('../../keys/sslcert/server.key', 'utf8');
-var certificate = fs.readFileSync('../../keys/server.crt', 'utf8');
+var privateKey  = fs.readFileSync('../../keys/privkey.pem', 'utf8');
+var certificate = fs.readFileSync('../../keys/cert.pem', 'utf8');
 
 var credentials = {key: privateKey, cert: certificate};
 
