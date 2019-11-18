7650f873bb289cfbb740e8dc075fc8a5894770a6
HDMcGrath <43671042+HDMcGrath@users.noreply.github.com>
2019-11-18 17:41:40 +0000

    added check cookies function

--- src/public/js/cookies.js
+++ src/public/js/cookies.js
@@ -21,3 +21,16 @@ function getCookies(cName, cID) {
         }
         return "";
 }
+
+function checkCookies(cName, cID) {
+        var user = getCookie("username");
+        if (user != "") {
+                alert("Welcome again " + user);
+        }
+        else {
+                user = prompt("Please enter your name:", "");
+                if (user != "" && user != null) {
+                        setCookie("username", user, 365);
+                }
+        }
+}
