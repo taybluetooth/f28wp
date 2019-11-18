c018742ca3f5ecf46950a2620a0185e9e7f21a39
HDMcGrath <43671042+HDMcGrath@users.noreply.github.com>
2019-11-18 00:46:32 +0000

    added getCookies method
    turns cookies into variables, should allow sending to db

--- src/public/js/cookies.js
+++ src/public/js/cookies.js
@@ -4,3 +4,20 @@ function setCookie(cName, cID, exdays) {
         var expires = "expires="+ d.toUTCString();
         document.cookie = cname + "=" + cID + ";" + expires + ";path=/"
 }
+
+function getCookies(cName, cID) {
+        var name = cname + "=";
+        var id = cID + "=";
+        var decodedCookie = decodeUSIComponent(document.cookie);
+        var ca = decodedCookie.split(";");
+        for (var i = 0; i < ca.length; i++) {
+                var c - ca[i];
+                while (c.charAt(0) == ' ') {
+                        c = c.substring(1);
+                }
+                if (c.indexOf(name) == 0) {
+                        return c.substring(name.length, c.length);
+                }
+        }
+        return "";
+}
