533a858d49749e27543e79fc80fe01a42b7a43c2
taybluetooth <callumtaylorbusiness@gmail.com>
2019-11-22 17:11:12 +0000

    Refactorization with Webpack server setup

---
+++ server-prototype/src/server/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const Game = require('/.game');
+const game = new Game();
+var path = require('path')
+var server = require('http').Server(game);
+var io = require('socket.io')(server, {});
+var socket_list = {}
+
+app.use('/client', express.static(__dirname + '/client'));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/client/html/index.html');
+});
+
+server.listen(process.env.port || 3000);
+console.log('BEST BOY TANK LISTENING ON PORT 3000');
+
+function joinGame(username) {
+  game.addPlayer(this, username);
+}
