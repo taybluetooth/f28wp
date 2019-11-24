1dc36014abc42653ae6ec513e9757c614a6566eb
taybluetooth <callumtaylorbusiness@gmail.com>
2019-11-24 16:51:01 +0000

    Changed node env

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
+var server_host = process.env.YOUR_HOST || ‘0.0.0.0’;
 
 module.exports = {
   entry: {
@@ -32,6 +34,14 @@ module.exports = {
         ],
       },
     ],
+  },
+  devServer: {
+    disableHostCheck: true,
+    contentBase: ‘./dist’,
+    compress: true,
+    inline: true,
+    port:server_port,
+    host:server_host
   },
   plugins: [
     new MiniCssExtractPlugin({
